34b89af6631a367aa4242e0771289ba17d23f809
He Liu <liulonnie@gmail.com>
2014-10-25 12:46:41 -0700

    .

--- www/xlangjs/main.go
+++ www/xlangjs/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	_ "github.com/h8liu/xlang/parser"
+	"bytes"
+	"fmt"
+
+	"github.com/h8liu/xlang/parser"
 )
 
 func main() {
 
+}
+
+func parseTokens(file, code string) string {
+	lex := parser.LexString(file, code)
+	out := new(bytes.Buffer)
+
+	for lex.Scan() {
+		t := lex.Token()
+		fmt.Fprintln(out, t)
+	}
+	return out.String()
 }
