cf6b2045aa0ff23739d672c3f3ba826c8b2df854
He Liu <liulonnie@gmail.com>
2014-11-14 10:06:15 -0800

    asmjs?

---
+++ www/asm-js.js
@@ -0,0 +1,13 @@
+function asmjs(stdlib, foreign, heap) {
+    "use asm";
+
+    var t = 0;
+
+    function f() {
+        switch (t|0) {
+        case 0:
+        default:
+        }
+    }
+    return {};
+}
