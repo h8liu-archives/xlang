d832e1ba6c1f77d92f5d366c22a7a69702e47ef0
He Liu <liulonnie@gmail.com>
2014-11-14 12:30:11 -0800

    asm js testing

--- www/asm-js.js
+++ www/asm-js.js
@@ -1,12 +1,38 @@
 function asmjs(stdlib, foreign, heap) {
     "use asm";
 
-    var t = 0;
+    var pc = 0;     // pseudo program counter
+    var sp = 0;     // stack pointer
+    var ret = 0;
+    var t0 = 0, t1 = 0, t2 = 0, t3 = 0; // temp 32-bit registers
+    var f0 = 0.0, f1 = 0.0, f2 = 0.0, f3 = 0.0; // temp floating point registers
 
-    function f() {
-        switch (t|0) {
-        case 0:
-        default:
+    var memI32 = new stdlib.Int32Array(heap);
+    var memU32 = new stdlib.Uint32Array(heap);
+    var memI8 = new stdlib.Int8Array(heap);
+    var memU8 = new stdlib.Uint8Array(heap);
+    var memF64 = new stdlib.Float64Array(heap);
+
+    function run(ncycle) {
+        ncycle = ncycle|0;
+
+        var nextpc = 0;
+
+        while (ncycle|0 > 0) {
+            nextpc = (pc + 4) | 0;
+
+            switch (pc|0) {
+            case 0: memU32[t0 >> 2] = t1|0; break; // memory access
+            case 1: if ((t1|0) == (t2|0)) { nextpc = 3|0; } break; // jmp
+            case 2: ret = nextpc; nextpc = ((nextpc|0) + 232) | 0; // call
+            case 3: nextpc = ret|0; // return
+            case 4: memU32[(sp + 4)>>2] = t1|0; break;
+            case 5: memF64[(sp + 8)>>3] = f0; break;
+            default: 
+            }
+
+            ncycle = ((ncycle|0) + -1)|0;
+            pc = nextpc;
         }
     }
     return {};
